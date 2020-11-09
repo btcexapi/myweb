9bee10b1a09fa69dc4cd9422bc9cb92a68f23eeb
pilipala1029 <1434351139@qq.com>
2020-11-09 20:38:26 +0800

    v0.4

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	})
 	r.GET("/user", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "user info v0.3",
+			"message": "user info v0.4",
 		})
 	})
 	r.Run(":8888") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
